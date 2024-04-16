e6a0d059d87cb7ea5f0ff10b110e759eb372e848
Rik Van Moll <rikvnmll@gmail.com>
2024-04-16 15:44:32 +0200

    Finished

--- calc.py
+++ calc.py
@@ -4,9 +4,16 @@ import sys
 
 def main():
     """Implement the calculator"""
-    pass  # YOUR CODE HERE
+    arg1 = int(sys.argv[1])
+    arg2 = int(sys.argv[3])
+    if sys.argv[2] == "+":
+        return arg1 + arg2
+    if sys.argv[2] == "*":
+        return arg1 * arg2
+    if sys.argv[2] == "-":
+        return arg1 - arg2
+
 
 
 if __name__ == "__main__":
     print(main())
-    
